8c5b98cf160e6b28eb47fe9400aec597f7c29dbb
AaronWu117 <itsaaron0000@gmail.com>
2025-10-26 14:40:42 -0400

    Update homework3.py

--- unit3/homework3.py
+++ unit3/homework3.py
@@ -1,1 +1,23 @@
+from random import randint
+# Generate a random number between 0 and 100
 
+
+tryagain = ("yes")
+
+while tryagain == "yes" or "Yes" or "YES" or "yES" or "YeS" or "yEs" or "YEs":
+    # Generate a random number between 0 and 100
+   
+    low = int(input("Enter the low number of the range: "))
+    high = int(input("Enter the high number of the range: "))
+    random_number = randint(low, high)
+
+    guess_num = int(input(f"Guess a number between {low} and {high}: "))
+
+    if random_number == guess_num:
+
+        print("You win!")
+
+    else:
+
+        print(f"You lose! The number was {random_number}" )
+        tryagain = input("Do you want to try again? (yes/no): ")
