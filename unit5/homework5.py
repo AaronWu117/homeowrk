bd29bdccfeaf37ceacc322731896538278821a21
AaronWu117 <itsaaron0000@gmail.com>
2025-12-14 17:26:12 -0500

    Add turtle graphics to draw shapes

--- unit5/homework5.py
+++ unit5/homework5.py
@@ -1,1 +1,46 @@
 
+import turtle
+
+turtle.bgcolor("black")
+turtle.speed(3)
+
+# Draw red square
+turtle.color("red")
+turtle.penup()
+turtle.goto(200,-200)
+turtle.pendown()
+turtle.begin_fill()
+
+turtle.forward(80)
+turtle.right(90)
+turtle.forward(80)
+turtle.right(90)
+turtle.forward(80)
+turtle.right(90)
+turtle.forward(80)
+turtle.end_fill()
+
+
+turtle.color("white")
+turtle.pensize(2)
+turtle.penup()
+turtle.goto(240,-200)
+turtle.pendown()
+
+for _ in range(30):       
+    turtle.forward(7)
+    turtle.left(1)
+    turtle.forward(7)
+    turtle.left(1)
+
+
+turtle.shape("circle")
+turtle.stamp()
+
+
+
+
+
+
+
+turtle.done()
